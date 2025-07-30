98d4c8016b4368dfd2df075356ae672c8246367f
Jason Fu <r.fu1@student.tue.nl>
2025-07-30 14:09:39 +0200

    comments

--- internal/actions/scripts.go
+++ internal/actions/scripts.go
@@ -11,44 +11,46 @@ const (
 	SCRIPTS_PATH = "/scripts"
 )
 
-// / will put the arguments of the method as arugments to all scripts
+// ExecuteScripts runs all executable files in the scripts directory,
+// passing IP change details and a message as arguments.
 func ExecuteScripts(previousIpv4 string, newIpv4 string, previousIpv6 string, newIpv6 string, message string) {
 	files, err := os.ReadDir(SCRIPTS_PATH)
 	if err != nil {
 		slog.Error("Unable to read scripts folder", "error", err)
+		return
 	}
 
 	for _, file := range files {
-		if !file.IsDir() {
-			fileInfo, err := file.Info()
-			if err != nil {
-				slog.Warn("Unable to check file permissions, continuing without checking")
-			} else {
-				// Check if file is executable by user
-				if fileInfo.Mode()&0111 == 0 {
-					// Not executable, try to add executable permission for user
-					newMode := fileInfo.Mode() | 0100 // Add user executable bit
-					scriptPath := filepath.Join(SCRIPTS_PATH, file.Name())
-					err := os.Chmod(scriptPath, newMode)
-					if err != nil {
-						slog.Error("Failed to make script executable", "script", file.Name(), "error", err)
-						continue // skip running this script
-					} else {
-						slog.Info("Made script executable", "script", file.Name())
-					}
+		if file.IsDir() {
+			continue
+		}
+
+		fileInfo, err := file.Info()
+		if err != nil {
+			slog.Warn("Unable to check file permissions, continuing without checking")
+		} else {
+			// Ensure the script is executable by the user
+			if fileInfo.Mode()&0111 == 0 {
+				newMode := fileInfo.Mode() | 0100
+				scriptPath := filepath.Join(SCRIPTS_PATH, file.Name())
+				if err := os.Chmod(scriptPath, newMode); err != nil {
+					slog.Error("Failed to make script executable", "script", file.Name(), "error", err)
+					continue
 				}
+				slog.Info("Made script executable", "script", file.Name())
 			}
+		}
 
-			scriptPath := filepath.Join(SCRIPTS_PATH, file.Name())
-			slog.Info("Executing script", "name", file.Name())
+		// Run the script with the IPs and message as arguments
+		scriptPath := filepath.Join(SCRIPTS_PATH, file.Name())
+		slog.Info("Executing script", "name", file.Name())
 
-			cmd := exec.Command(scriptPath, previousIpv4, newIpv4, previousIpv6, newIpv6, message)
-			cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
+		cmd := exec.Command(scriptPath, previousIpv4, newIpv4, previousIpv6, newIpv6, message)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
 
-			if err := cmd.Run(); err != nil {
-				slog.Error("Failed to run script", "script", file.Name(), "error", err)
-			}
+		if err := cmd.Run(); err != nil {
+			slog.Error("Failed to run script", "script", file.Name(), "error", err)
 		}
 	}
 
--- internal/actions/smtp.go
+++ internal/actions/smtp.go
@@ -8,14 +8,15 @@ import (
 	"github.com/fasonju/ipNotify/internal/types"
 )
 
+// NotifySMTP sends an email with the given message using SMTP.
 func NotifySMTP(cfg *types.Config, message string) {
 	subject := "Subject: Ip update notification\r\n"
 	mime := "MIME-version: 1.0;\r\nContent-Type: text/plain; charset=\"UTF-8\";\r\n\r\n"
 
 	hostAddr := cfg.SmtpServer + ":" + fmt.Sprint(cfg.SmtpPort)
 	slog.Info("Sending Email", "mail", message)
-	auth := smtp.PlainAuth("", cfg.SmtpUsername, cfg.SmtpPassword, cfg.SmtpServer)
 
+	auth := smtp.PlainAuth("", cfg.SmtpUsername, cfg.SmtpPassword, cfg.SmtpServer)
 	mail := []byte(subject + mime + "\r\n" + message)
 
 	err := smtp.SendMail(hostAddr, auth, cfg.SmtpFrom, []string{cfg.SmtpTo}, mail)
